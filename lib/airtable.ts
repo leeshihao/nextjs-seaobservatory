f0aa7e4080204ce1e2f8811496cfd19197807b03
leeshihao <leeshihao1@gmail.com>
2024-11-08 15:39:36 -0500

    changed base

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -4,7 +4,7 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
   .base(process.env.AIRTABLE_BASE_ID as string)
 
 export async function getPolicies(country?: string) {
-  const records = await base('Policies')
+  const records = await base('Observatory')
     .select({
       filterByFormula: country ? `{Country} = '${country}'` : '',
     })
